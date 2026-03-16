9990510965e83daed4b8161035fe3a93af79511e
prerna-egov <prerna.kumari@equidhi.org>
2026-03-16 19:02:48 +0530

    added dimension tables

---
+++ ddl/07_dimensions.sql
@@ -0,0 +1,77 @@
+CREATE TABLE IF NOT EXISTS punjab_property_tax.dimension_tenant_id
+(
+    -- created time 
+    created_time DateTime64(3) DEFAULT now64(3),
+
+    -- dimension columns
+    code String,
+    locale LowCardinality(String),
+    value String
+
+)
+ENGINE = ReplacingMergeTree(created_time)
+ORDER BY (locale, code)
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE IF NOT EXISTS punjab_property_tax.dimension_property_type
+(
+    -- created time 
+    created_time DateTime64(3) DEFAULT now64(3),
+
+    -- dimension columns
+    code String,
+    locale LowCardinality(String),
+    value String
+
+)
+ENGINE = ReplacingMergeTree(created_time)
+ORDER BY (locale, code)
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE IF NOT EXISTS punjab_property_tax.dimension_usage_category
+(
+    -- created time 
+    created_time DateTime64(3) DEFAULT now64(3),
+
+    -- dimension columns
+    code String,
+    locale LowCardinality(String),
+    value String
+
+)
+ENGINE = ReplacingMergeTree(created_time)
+ORDER BY (locale, code)
+SETTINGS index_granularity = 8192;
+
+
+
+CREATE TABLE IF NOT EXISTS punjab_property_tax.dimension_ownership_category
+(
+    -- created time 
+    created_time DateTime64(3) DEFAULT now64(3),
+
+    -- dimension columns
+    code String,
+    locale LowCardinality(String),
+    value String
+
+)
+ENGINE = ReplacingMergeTree(created_time)
+ORDER BY (locale, code)
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE IF NOT EXISTS punjab_property_tax.dimension_payment_mode
+(
+    -- created time 
+    created_time DateTime64(3) DEFAULT now64(3),
+
+    -- dimension columns
+    code String,
+    locale LowCardinality(String),
+    value String
+
+)
+ENGINE = ReplacingMergeTree(created_time)
+ORDER BY (locale, code)
+SETTINGS index_granularity = 8192;
+
