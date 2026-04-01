f7b7e2dd00c456fac08838ab0a392e2a848f6dd3
prerna-egov <prerna.kumari@equidhi.org>
2026-04-01 17:26:52 +0530

    added the dimension tables for boundary codes

--- ddl/07_dimensions.sql
+++ ddl/07_dimensions.sql
@@ -75,3 +75,49 @@ ENGINE = ReplacingMergeTree(created_time)
 ORDER BY (locale, code)
 SETTINGS index_granularity = 8192;
 
+CREATE TABLE IF NOT EXISTS punjab_property_tax.dimension_locality
+(
+    -- created time 
+    created_time DateTime64(3) DEFAULT now64(3),
+
+    -- dimension columns
+    code String,
+    locale LowCardinality(String),
+    value String
+
+)
+ENGINE = ReplacingMergeTree(created_time)
+ORDER BY (locale, code)
+SETTINGS index_granularity = 8192;
+
+
+CREATE TABLE IF NOT EXISTS punjab_property_tax.dimension_zone
+(
+    -- created time 
+    created_time DateTime64(3) DEFAULT now64(3),
+
+    -- dimension columns
+    code String,
+    locale LowCardinality(String),
+    value String
+
+)
+ENGINE = ReplacingMergeTree(created_time)
+ORDER BY (locale, code)
+SETTINGS index_granularity = 8192;
+
+
+CREATE TABLE IF NOT EXISTS punjab_property_tax.dimension_block
+(
+    -- created time 
+    created_time DateTime64(3) DEFAULT now64(3),
+
+    -- dimension columns
+    code String,
+    locale LowCardinality(String),
+    value String
+
+)
+ENGINE = ReplacingMergeTree(created_time)
+ORDER BY (locale, code)
+SETTINGS index_granularity = 8192;
